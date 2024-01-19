3eec642b48abe72bd6ca17ae344ab1636ab9871b
Leo Alves <leoalves0698@gmail.com>
2024-01-19 16:18:53 -0300

    feat: add system logout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { FaHome, FaSignInAlt, FaUserAlt, FaCircle } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { FaHome, FaSignInAlt, FaUserAlt, FaCircle, FaPowerOff } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
+import * as actions from "../../store/modules/auth/actions";
 import { Nav } from "./styled";
 // eslint-disable-next-line react-refresh/only-export-components, react/display-name
 export default () => {
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = (e) => {
+      e.preventDefault();
+      dispatch(actions.loginFailure());
+      navigate("/");
+    }
   return (
     <>
       <Nav>
@@ -16,10 +26,16 @@ export default () => {
         <Link to="/Register">
           <FaUserAlt size={24} />
         </Link>
-        <Link to="/login">
+        {isLoggedIn ? (
+          <Link onClick={handleLogout} to="/login">
+          <FaPowerOff size={24} />
+          </Link>
+        ) : (
+          <Link to="/login">
           <FaSignInAlt size={24} />
-        </Link>
-        <FaCircle size={24} className="status-login"/>
+          </Link>
+        ) }
+        {isLoggedIn &&  <FaCircle size={24} className="status-login"/>}
       </Nav>
     </>
   )
